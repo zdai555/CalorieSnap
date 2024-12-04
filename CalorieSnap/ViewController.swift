1e4c01eb75e0cfce0849f89ba36abef650c993ad
zdai555 <dai.zew@northeastern.edu>
2024-12-03 21:53:34 -0600

    Initial Commit

---
+++ CalorieSnap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalorieSnap
+//
+//  Created by Zewei Dai on 12/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
